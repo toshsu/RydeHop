2233761ba30d79233cd6281b3591c221d18b3535
toshani <135027437+toshsu@users.noreply.github.com>
2025-10-26 20:53:48 +0530

    Update trie.h

--- src/data_structures/trie.h
+++ src/data_structures/trie.h
@@ -1,3 +1,25 @@
+/*#ifndef TRIE_H
+#define TRIE_H
+
+#define ALPHABET_SIZE 26
+
+typedef struct TrieNode {
+    struct TrieNode *children[ALPHABET_SIZE];
+    int is_end_of_word;
+    void *data;
+} TrieNode;
+
+typedef struct {
+    TrieNode *root;
+} Trie;
+
+Trie* create_trie();
+void trie_insert(Trie *trie, char *key, void *data);
+void* trie_search(Trie *trie, char *key);
+void free_trie(Trie *trie);
+
+#endif
+*/
 #ifndef TRIE_H
 #define TRIE_H
 
